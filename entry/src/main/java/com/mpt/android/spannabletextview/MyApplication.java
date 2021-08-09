c55e4bb3469d09e6e01e2b907064974d849495f4
Sujith <kumarsujith683@gmail.com>
2021-08-09 08:32:17 -0700

    Modified few files

---
+++ entry/src/main/java/com/mpt/android/spannabletextview/MyApplication.java
@@ -0,0 +1,14 @@
+package com.mpt.android.spannabletextview;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+/**
+ * Default MainApplication class to launch the Application.
+ */
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
